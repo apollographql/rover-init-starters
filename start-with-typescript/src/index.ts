e0289d109aea59c9da921fb03a8d9949c2e2e6b8
Daniel Sanchez <daniel.sanchez@apollographql.com>
2025-04-28 17:36:59 -0400

    REmoving environment variable and rover dev prompt

--- start-with-typescript/src/index.ts
+++ start-with-typescript/src/index.ts
@@ -10,27 +10,8 @@ import resolvers from "./resolvers";
 import { DataSourceContext } from "./types/DataSourceContext";
 import { GraphQLError } from "graphql";
 
-const port = process.env.PORT ?? "4001";
-const subgraphName = require("../package.json").name;
-const routerSecret = process.env.ROUTER_SECRET;
-
-const context: ContextFunction<
-  [StandaloneServerContextFunctionArgument],
-  DataSourceContext
-> = async ({ req }) => {
-  if (routerSecret && req.headers["router-authorization"] !== routerSecret) {
-    throw new GraphQLError("Missing router authentication", {
-      extensions: {
-        code: "UNAUTHENTICATED",
-        http: { status: 401 },
-      },
-    });
-  }
-
-  return {
-    auth: req.headers.authorization,
-  };
-};
+const port = "4001";
+const subgraphName = "things";
 
 async function main() {
   let typeDefs = gql(
@@ -42,12 +23,10 @@ async function main() {
     schema: buildSubgraphSchema({ typeDefs, resolvers }),
   });
   const { url } = await startStandaloneServer(server, {
-    context,
     listen: { port: Number.parseInt(port) },
   });
 
   console.log(`🚀  Subgraph ${subgraphName} ready at ${url}`);
-  console.log(`Run rover dev --url ${url} --name ${subgraphName}`);
 }
 
 main();
